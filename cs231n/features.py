639395e147f276865416eaa3dc2a8ed0b61e8595
Yang Bo <atryyang@thoughtworks.com>
2016-06-09 01:45:25 +0800

    Port to Python 3

--- cs231n/features.py
+++ cs231n/features.py
@@ -41,7 +41,7 @@ def extract_features(imgs, feature_fns, verbose=False):
   imgs_features[0] = np.hstack(first_image_features).T
 
   # Extract features for the rest of the images.
-  for i in xrange(1, num_images):
+  for i in range(1, num_images):
     idx = 0
     for feature_fn, feature_dim in zip(feature_fns, feature_dims):
       next_idx = idx + feature_dim
